b258c0c00b43d5f578bd958510fd12ead864363b
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2019-02-06 09:32:47 -0500

    Add code for high submission, high submission itself

--- assignment_1/batchnorm.py
+++ assignment_1/batchnorm.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 class CustomBatchNorm2D(nn.Module):
     def __init__(self, dim, lr=0.99):
         super().__init__()
-        self.beta = nn.Parameter(T.ones(dim), requires_grad=True)
-        self.alpha = nn.Parameter(T.zeros(dim), requires_grad=True)
+        self.beta = nn.Parameter(T.ones((1, dim, 1, 1)), requires_grad=True)
+        self.alpha = nn.Parameter(T.zeros((1, dim, 1, 1)), requires_grad=True)
 
         self.mean_history = nn.Parameter(T.zeros(dim), requires_grad=False)
         self.std_history = nn.Parameter(T.ones(dim), requires_grad=False)
@@ -14,20 +14,19 @@ class CustomBatchNorm2D(nn.Module):
         self.lr = lr
 
     def forward(self, x):
-        flat_x = x.permute(0, 2, 3, 1).contiguous().view(-1, x.shape[1])
         if self.training:
-            mean = flat_x.mean(0)
-            std = flat_x.std(0)
+            mean = x.sum((0, 2, 3), keepdim=True)/(x.shape[0]*x.shape[2]*x.shape[3])
+            x_centered = x - mean
+            std = T.sqrt((x_centered**2).sum((0, 2, 3), keepdim=True)/(x.shape[0]*x.shape[2]*x.shape[3]))
             self.mean_history.data = self.mean_history.data*self.lr + mean.detach()*(1 - self.lr)
             self.std_history.data = self.std_history.data*self.lr + std.detach()*(1 - self.lr)
         else:
             mean = self.mean_history
             std = self.std_history
+            x_centered = x - mean
 
-        normed_x = (flat_x - mean)/(std + 0.000001)
+        normed_x = (x_centered)/(std + 0.000001)
         scaled_x = normed_x * self.beta + self.alpha
 
-        reshaped_x = scaled_x.view(x.shape[0], x.shape[2], x.shape[3], x.shape[1]).contiguous().permute(0, 3, 1, 2)
-
-        return reshaped_x
+        return scaled_x
 
